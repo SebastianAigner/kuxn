69c87dcb4a44b657770cf7a5b490c68f564b4743
Sebastian Aigner <sebastian.aigner@tum.de>
2022-11-26 17:49:51 +0100

    🚀 Get the first Uxn program to run: Support LIT, ADD, DEO, BRK instructions Implement nicer debug view for memory (cut out zero-only areas) Add more interesting notes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # kuxn
 Experiments with emulating the [Uxn personal computing environment](https://100r.co/site/uxn.html) created by [100R](https://100r.co/site/about_us.html) using Kotlin, built live at https://twitch.tv/sebi_io!
+
+
+## Notes from the stream
+
+### Dev plan
+
+- How does the stack work?
+- Implement minimal set of instructions (`LIT`, `ADD`, `DEO`)
+- Build immutable version of the `UxnMachine` using [kx.collections.immutable](https://github.com/Kotlin/kotlinx.collections.immutable)
+
+### Important resources
+
+- [Uxn main page](https://100r.co/site/uxn.html)
+- [Varvara](https://wiki.xxiivv.com/site/varvara.html), the "computer" surrounding the Uxn CPU
+- [Uxn tutorial, multi-part](https://compudanzas.net/uxn_tutorial_day_1.html)
+- [CPU instruction tests](https://github.com/DeltaF1/uxn-instruction-tests)
+
+### Minimal viable Varvara
+Device Address `0x18` (Console Write)
--- src/main/kotlin/UxnThing.kt
+++ src/main/kotlin/UxnThing.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import Instruction.*
 
 fun main() {
     val program = File("simpleadd.tal.rom").readBytes().toUByteArray()
@@ -6,23 +7,101 @@ fun main() {
     val machine = UxnMachine()
     machine.loadRom(program)
     machine.debugPrint()
+    var machineState: MachineState = MachineState.RUNNING
+    while(machineState != MachineState.STOPPED) {
+        machineState = machine.step()
+    }
+}
+
+enum class MachineState {
+    RUNNING,
+    STOPPED
 }
 
 class UxnMachine {
-    private val memory: MutableList<UByte> = MutableList(0xFFFF + 1) { 0u }
+    private val memory: MutableList<UByte> = MutableList(65536) { 0u }
+    private val workingStack: ArrayDeque<UByte> = ArrayDeque(256)
+    private var ip: UShort = 0x100u
     fun debugPrint() {
         // 0x100: 80
         // 0x101: 0
         // ...
         // 0x205: 92
-        memory.forEachIndexed { index, uByte ->
-            println("0x${index.toString(16)}: ${uByte.toString(16)}")
+        var zeroes = 0
+        var shouldPrintEllipsis = true
+        for(index in memory.indices) {
+            val address = "0x${index.toString(16)}"
+            val byte = memory[index]
+            val value = byte.toString(16)
+            if(byte == 0u.toUByte()) {
+                zeroes++
+            } else {
+                zeroes = 0
+                shouldPrintEllipsis = true
+            }
+            if(zeroes > 5) {
+                if(shouldPrintEllipsis) {
+                    println("...")
+                    shouldPrintEllipsis = false
+                }
+                continue
+            }
+            println("$address: $value")
         }
     }
+
+    fun step(): MachineState {
+        if(ip.toInt() > memory.lastIndex) {
+            println("Reached end of memory, terminating.")
+            return MachineState.STOPPED
+        }
+        val instruction = memory[ip].toInstruction()
+        ip++
+        when(instruction) {
+            LIT -> {
+                workingStack.addLast(memory[ip])
+                ip++
+            }
+            ADD -> {
+                val a = workingStack.removeLast()
+                val b = workingStack.removeLast()
+                workingStack.addLast((a + b).toUByte())
+            }
+            DEO -> {
+                val device = workingStack.removeLast()
+                require(device == 0x18u.b) { "Currently only device known is console output (0x18)" }
+                print(workingStack.removeLast().toInt().toChar())
+            }
+            BRK -> {
+                return MachineState.STOPPED
+            }
+        }
+        return MachineState.RUNNING
+    }
+
     fun loadRom(rom: UByteArray) {
         // load rom at 0x100
         rom.forEachIndexed { idx, byte ->
             memory[0x100 + idx] = byte
         }
     }
+}
+
+enum class Instruction(val byte: UByte) {
+    LIT(0x80u.b),
+    ADD(0x18u.b),
+    DEO(0x17u.b),
+    BRK(0x0u.b)
+}
+
+val UInt.b: UByte get() {
+    return this.toUByte()
+}
+
+fun UByte.toInstruction(): Instruction {
+    return Instruction.values().find { it.byte == this } ?: error("Unknown instruction ${this.toString(16)}")
+}
+
+private operator fun <T> List<T>.get(uShort: UShort): T {
+    return get(uShort.toInt())
 }
